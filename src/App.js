da030425f04e584a3aa85158c01a964bc2a9b0e1
Kevin Kerin <kevin.kerin@gmail.com>
2020-06-24 18:18:52 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {
+      loading: true,
+      person: null,
+      counter: 0
+    }
+    this.setCounter = this.setCounter.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me"
+    const response = await fetch(url)
+    const data = await response.json();
+    // this.state.person = data.results[0]
+    this.setState({
+      person: data.results[0],
+      loading: false
+    })
+    console.log(this.state.person.location.city)
+    console.log(this.state.person.location.state)
+    console.log(this.state.person.location.country)
+
+  }
+  componentDidUpdate() {
+    document.title = `You clicked ${this.state.counter} times`;
+  }
+  
+  setCounter(){
+    this.setState((prevState) => {
+      return {
+        counter: (prevState.counter + 1)
+      }
+    })
+  }
+
+  render() {
+    return (<div className="App">
+      {this.state.loading ? (
+        <div>Loading...</div>
+      ) : <div>{this.state.person.name.first} {this.state.person.name.last}
+      <img src={this.state.person.picture.large}/></div>}
+    </div>)
+
+}
+}
 export default App;
